6249672ad811e5911dc3cade0e18a3b0428d93ad
Nini2333 <1010416362@qq.com>
2019-04-15 15:37:56 +0800

    Add

---
+++ Test_190412/Assets/Scripts/Player.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 8;
+
+    private Transform shotPosTrans;
+
+    public GameObject bullet;
+
+    private GameObject explosionPrefab;
+
+    public int hp = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        explosionPrefab = Resources.Load<GameObject>("Prefabs/PlayerDieEffect");
+
+        shotPosTrans = transform.Find("ShotPos");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        transform.position += (Vector3.right * h + Vector3.up * v) * speed * Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            Shoot();
+        }
+
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            Hurt();
+        }
+
+    }
+
+    void Shoot()
+    {
+        Instantiate(bullet, shotPosTrans.position, Quaternion.identity);
+    }
+
+
+    void Hurt()
+    {
+        if (hp <= 0)
+        {
+            Destroy(gameObject);
+
+        }
+        Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
